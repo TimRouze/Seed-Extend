3b089ac3fe77490c01761a61ab07a3a078307441
Tim <timothe.rouze1@gmail.com>
2022-03-22 08:16:21 +0100

    Added find_seeds, needs to be tested properly but theoretically it works lol Also added the suffix array creation and this one works for sure.

--- SeedExtend.py
+++ SeedExtend.py
@@ -1,5 +1,15 @@
 import sys, argparse
 from Bio import SeqIO
+from Utils import *
+
+def find_Seeds(kmers, sequence, suffix_array, k):
+    seeds = {}
+    for kmer in kmers:
+        first = dichotomicSearchFirst(kmer, sequence, suffix_array, k)
+        last = dichotomicSearchLast(kmer, sequence, suffix_array, k)
+        for i in range(first, last+1):
+            seeds[kmer].append(suffix_array[i])
+    return seeds
 
 def main():
     #Command line options
@@ -9,6 +19,12 @@ def main():
     parser.add_argument('-o', '--out', required=True, metavar='', help='Output file')
     parser.add_argument('-k', '--kmersize', required=True, metavar='', help='size of the kmers')
     args = parser.parse_args(sys.argv[1:])
-    
+    sequences = parseFasta(args.genome)
+    queries = parseFasta(args.reads)
+    kmers = find_kmers(queries[0],args.kmersize)
+    suffix_array = create_suffix_array(sequences[0].seq, args.kmersize)
+    print(len(suffix_array))
+    print(len(sequences[0].seq))
+
 if __name__=="__main__":
     main()
--- Utils.py
+++ Utils.py
@@ -10,13 +10,13 @@ def parseFasta(fi):
             print('File format incorrect or file content does not correspond to fasta.')
     return sequences
 
-def kmers(seq, k):
+def find_kmers(seq, k):
     sequence = seq.seq
     kmers = []
     i = 0
     while i < len(sequence) - k:
-        if len(sequence[i:i+k]) == k:
-            kmers.append(str(sequence[i:i+k]))
+        if len(sequence[i:i+k]) == k: #and sequence[i:i+k] not in kmers:
+            kmers.append(sequence[i:i+k]) 
         i += k
     return kmers
 
@@ -29,3 +29,35 @@ def reverseComplement(seq):
         for i in seq:
             rc += pairs[i]  #Pour chaque nucléotide, insérer son complément à la suite du complément des précédentes.
         return rc[::-1]
+
+def create_suffix_array(sequence, k):
+    #sequence.append("$")
+    suffix_array = list(range(len(sequence)-k))
+    suffix_array.sort(key = lambda i: sequence[i:])
+    return suffix_array
+
+def dichotomicSearchFirst(kmer, sequence, suffix_array, k):
+    first = 0
+    last = len(suffix_array)-1
+    while first <= last:
+        mid = first + (last - first) // 2
+        if sequence[suffix_array[mid]:suffix_array[mid]+k] == kmer and sequence[suffix_array[mid-1]:suffix_array[mid-1]+k] < kmer:
+            return mid
+        elif sequence[suffix_array[mid]:suffix_array[mid]+k] < x:
+            first = mid + 1
+        else:
+            last = mid - 1
+    return first
+
+def dichotomicSearchLast(kmer, sequence, suffix_array, k):
+    first = 0
+    last = len(suffix_array)-1
+    while first <= last:
+        mid = first + (last - first) // 2
+        if sequence[suffix_array[mid]:suffix_array[mid]+k] == kmer and sequence[suffix_array[mid+1]:suffix_array[mid+1]+k] > kmer:
+            return mid
+        elif sequence[suffix_array[mid]:suffix_array[mid]+k] < x:
+            first = mid + 1
+        else:
+            last = mid - 1
+    return last
